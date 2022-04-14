6bf3b915d1ba9118fb062eeb2fa85c436e6634e0
Krintox <shashanksuggala@gmail.com>
2022-04-14 18:33:33 +0530

    First Commit

---
+++ Scripts/index.js
@@ -0,0 +1,79 @@
+
+
+function progressDisp1() {
+    document.getElementById("level-count").style.display = "block";
+    document.getElementById("first").style.width = "40%";
+    document.getElementById("htmlcssjs").style.display = "block";
+    document.getElementById("react").style.display = "none";
+    document.getElementById("python").style.display = "none";
+    document.getElementById("node").style.display = "none";
+    document.getElementById("mysql").style.display = "none";
+    document.getElementById("c-prog").style.display = "none";
+    document.getElementById("cpp-prog").style.display = "none";
+}
+function progressDisp2() {
+    document.getElementById("level-count").style.display = "block";
+    document.getElementById("first").style.width = "30%";
+    document.getElementById("htmlcssjs").style.display = "none";
+    document.getElementById("react").style.display = "block";
+    document.getElementById("python").style.display = "none";
+    document.getElementById("node").style.display = "none";
+    document.getElementById("mysql").style.display = "none";
+    document.getElementById("c-prog").style.display = "none";
+    document.getElementById("cpp-prog").style.display = "none";
+}
+function progressDisp3() {
+    document.getElementById("level-count").style.display = "block";
+    document.getElementById("first").style.width = "25%";
+    document.getElementById("htmlcssjs").style.display = "none";
+    document.getElementById("react").style.display = "none";
+    document.getElementById("python").style.display = "block";
+    document.getElementById("node").style.display = "none";
+    document.getElementById("mysql").style.display = "none";
+    document.getElementById("c-prog").style.display = "none";
+    document.getElementById("cpp-prog").style.display = "none";
+}
+function progressDisp4() {
+    document.getElementById("level-count").style.display = "block";
+    document.getElementById("first").style.width = "5%";
+    document.getElementById("htmlcssjs").style.display = "none";
+    document.getElementById("react").style.display = "none";
+    document.getElementById("python").style.display = "none";
+    document.getElementById("node").style.display = "block";
+    document.getElementById("mysql").style.display = "none";
+    document.getElementById("c-prog").style.display = "none";
+    document.getElementById("cpp-prog").style.display = "none";
+}
+function progressDisp5() {
+    document.getElementById("level-count").style.display = "block";
+    document.getElementById("first").style.width = "6%";
+    document.getElementById("htmlcssjs").style.display = "none";
+    document.getElementById("react").style.display = "none";
+    document.getElementById("python").style.display = "none";
+    document.getElementById("node").style.display = "none";
+    document.getElementById("mysql").style.display = "block";
+    document.getElementById("c-prog").style.display = "none";
+    document.getElementById("cpp-prog").style.display = "none";
+}
+function progressDisp6() {
+    document.getElementById("level-count").style.display = "block";
+    document.getElementById("first").style.width = "8%";
+    document.getElementById("htmlcssjs").style.display = "none";
+    document.getElementById("react").style.display = "none";
+    document.getElementById("python").style.display = "none";
+    document.getElementById("node").style.display = "none";
+    document.getElementById("mysql").style.display = "none";
+    document.getElementById("c-prog").style.display = "block";
+    document.getElementById("cpp-prog").style.display = "none";
+}
+function progressDisp7() {
+    document.getElementById("level-count").style.display = "block";
+    document.getElementById("first").style.width = "5%";
+    document.getElementById("htmlcssjs").style.display = "none";
+    document.getElementById("react").style.display = "none";
+    document.getElementById("python").style.display = "none";
+    document.getElementById("node").style.display = "none";
+    document.getElementById("mysql").style.display = "none";
+    document.getElementById("c-prog").style.display = "none";
+    document.getElementById("cpp-prog").style.display = "block";
+}
